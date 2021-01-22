1353228d242b42a88dd4ebe80a6a259a204cac69
Arthur Chen <arthur@MacBook-Pro-de-Arthur.local>
2021-01-22 18:17:44 +0100

    add console.log(req) if no req.files.picture

--- middlewares/uploadPicture.js
+++ middlewares/uploadPicture.js
@@ -4,6 +4,7 @@ const uploadPicture = async (req, res, next) => {
       return next();
     } else {
       res.status(400).json({ error: { message: "Missing picture" } });
+      console.log(req);
     }
   } catch (error) {
     res.status(400).json({ error: error.message });
